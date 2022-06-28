5dcaba6826d5d088f77b9c7a6fa730427573f49f
ndikumanaisaie <ndikumanaisaie@gmail.com>
2022-06-28 19:19:25 +0100

    Testing the useState hook

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const App = () => {
   const [name, setName] = useState('name');
