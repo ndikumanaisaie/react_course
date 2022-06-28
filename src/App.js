3fdf404b03881b61307215020bd4d6f635cf42b8
ndikumanaisaie <ndikumanaisaie@gmail.com>
2022-06-28 13:22:52 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react';
+
+const App = () => {
+  const [name, setName] = useState('name');
+  return (
+    <div className="first-div">
+      <p>Your name is { name }</p>
+      <button onClick={ () => setName('Ndikumana Isaie')}>
+        display name
+      </button>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import ReactDom from 'react-dom';
+
+import './styles/style.css';
+import App from './App.js';
+
+ReactDom.render(<App />, document.getElementById('root'));
